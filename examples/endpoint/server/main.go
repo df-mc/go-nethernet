16e4ec8d63cfcec3e24dec4f6d38eeb354e9e39b
lactyy2 <284460485+lactyy2@users.noreply.github.com>
2026-09-16 02:00:25 +0900

    examples/endpoint/server: Remove warning when listening on plain HTTP

--- examples/endpoint/server/main.go
+++ examples/endpoint/server/main.go
@@ -75,8 +75,6 @@ func main() {
 		if enableTLS {
 			err = srv.ServeTLS(tcp, certFile, keyFile)
 		} else {
-			// We still support listening on plain HTTP to allow use of reverse proxy like Caddy.
-			slog.Warn("listening on plain HTTP. vanilla clients may not be able to connect this server as it always attempt to connect with HTTPS")
 			err = srv.Serve(tcp)
 		}
 		if err != nil && !errors.Is(err, net.ErrClosed) && !errors.Is(err, http.ErrServerClosed) {
